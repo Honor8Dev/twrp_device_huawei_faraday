5c96b73665f27c295903eb75f982fe74e8e6e09e
Muhammad Fahad Baig <muhammad.fb.79@gmail.com>
2016-12-12 23:01:53 +0500

    add sepolicy from B320 and apply hacks

--- faraday.mk
+++ faraday.mk
@@ -3,6 +3,12 @@ LOCAL_PATH := device/huawei/faraday
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# Hackery
+$(shell mkdir -p $(OUT)/obj/ETC/sepolicy.recovery_intermediates)
+$(shell mkdir -p $(OUT)obj/ETC/sepolicy_intermediates) 
+$(shell cp device/huawei/faraday/sepolicy/prebuilt/sepolicy $(OUT)/obj/ETC/sepolicy.recovery_intermediates/sepolicy.recovery)
+$(shell cp device/huawei/faraday/sepolicy/prebuilt/sepolicy $(OUT)/obj/ETC/sepolicy_intermediates/sepolicy)
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
